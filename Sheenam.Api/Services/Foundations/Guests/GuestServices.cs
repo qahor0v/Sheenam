a8ec18f25e63ad936ee1345e59c503741eb92776
qahor0v <fluttuzgroup@gmail.com>
2023-09-02 01:49:11 +0500

    last changes 9.2.2023-1-49

---
+++ Sheenam.Api/Services/Foundations/Guests/GuestServices.cs
@@ -0,0 +1,16 @@
+﻿using Sheenam.Api.Brokers.Storages;
+using Sheenam.Api.Models.Foundation.Guests;
+
+namespace Sheenam.Api.Services.Foundations.Guests
+{
+    public class GuestServices : IGuestServices
+    {
+        private readonly IStorageBroker storageBroker;
+
+        public GuestServices(IStorageBroker storageBroker) => this.storageBroker = storageBroker;
+        public ValueTask<Guest> AddGuestAsync(Guest guest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Sheenam.Api/Services/Foundations/Guests/IGuestServices.cs
@@ -0,0 +1,8 @@
+﻿using Sheenam.Api.Models.Foundation.Guests;
+
+namespace Sheenam.Api.Services.Foundations.Guests;
+
+public interface IGuestServices
+{
+    ValueTask<Guest> AddGuestAsync(Guest guest);
+}
