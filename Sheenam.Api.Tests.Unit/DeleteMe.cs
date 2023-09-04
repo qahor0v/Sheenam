749b12b7d5aa65ca012e8beb2d67d5ecffd52006
qahor0v <fluttuzgroup@gmail.com>
2023-09-04 15:01:42 +0500

    unit test added

---
+++ Sheenam.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,15 @@
+﻿//===============================================================
+//Copyright (c) Qahorov
+//Free to use Comfort and peace
+//===============================================================
+
+using Xunit;
+
+namespace Sheenam.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue()=>Assert.True(true);  
+    }
+}
