761b57fe779eb7aa427079ee0235e62eec5c6ea5
qahor0v <fluttuzgroup@gmail.com>
2023-09-04 14:52:44 +0500

    project resetted

---
+++ Sheenam.Api/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿//===============================================================
+//Copyright (c) Qahorov
+//Free to use Comfort and peace
+//===============================================================
+
+
+
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace Sheenam.Api.Controllers
+
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok("Hello Flutter!");
+        }
+
+
+
+    }
+}
